55a56e9c6c70bdaac0473d7192a8c56d2c797306
Erwin Holzhauser <erwinholzhauser@gmail.com>
2022-07-25 19:31:34 -0400

    Use pandas df as data store for powerball data

--- rest_lottery_exercise/app.py
+++ rest_lottery_exercise/app.py
@@ -1,61 +1,69 @@
-import csv
 import datetime
 import os
 from pprint import pprint
 
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
-_draws_list = []
+import numpy as np
+import pandas as pd
 
 
-def _populate_draws_list():
-    input_file = os.path.join(os.path.dirname(
+def _dataframe_from_file():
+    filepath = os.path.join(os.path.dirname(
         __file__), 'input/powerball_winning_numbers.csv')
-    with open(input_file) as csv_file:
-        reader = csv.DictReader(csv_file)
-        for draw in reader:
-            draw['draw_date'] = draw.pop('Draw Date').split('/')
-            draw['draw_date'] = datetime.datetime(
-                *[int(draw['draw_date'][i]) for i in [2, 0, 1]])
 
-            draw['multiplier'] = draw.pop('Multiplier')
-            draw['multiplier'] = 1.0 if draw['multiplier'] == '' else float(
-                draw['multiplier'])
+    df = pd.read_csv(filepath)
+    df.columns = ['date', 'winning_numbers', 'multiplier']
 
-            draw['winning_numbers'] = [
-                int(i) for i in draw.pop('Winning Numbers').split(' ')]
+    df['date'] = [datetime.datetime.strptime(
+        i, '%m/%d/%Y') for i in df['date']]
 
-            # pprint(draw)
-            _draws_list.append(draw)
+    df['winning_numbers'] = [[int(j) for j in i.split(' ')]
+                             for i in df['winning_numbers']]
 
+    # df['multiplier'].fillna(1.0, inplace=True)
 
-def _get_winning_nums_from_date(start, end=None, draws=_draws_list):
-    start_split = start.split('/')
-    start_dt = datetime.datetime(*[int(start_split[i]) for i in [2, 0, 1]])
+    pprint(df)
 
-    if end is None:
-        end_dt = start_dt
-    else:
-        end_split = end.split('/')
-        end_dt = datetime.datetime(*[int(end_split[i]) for i in [2, 0, 1]])
-
-    return [i['winning_numbers'] for i in draws
-            if i['draw_date'] >= start_dt and i['draw_date'] <= end_dt]
+    return df
 
 
-def _get_draws_from_multiplier(mult, draws=_draws_list):
-    return [i for i in draws if i['multiplier'] == mult]
+def _winning_numbers_from_dates(df, start, end=None):
+    start = datetime.datetime.strptime(start, '%m/%d/%Y')
+    end = datetime.datetime.strptime(
+        end, '%m/%d/%Y') if end is not None else start
+
+    return df.loc[(df['date'] >= start) & (df['date'] <= end)]['winning_numbers']
 
 
-@app.route('/draw/', methods=['GET'])
-def get_draws():
-    pass
+def _draws_from_multiplier(df, mult):
+    return df.loc[df['multiplier'] == mult]
+
+
+def _dates_from_winning_numbers(df, winning_nums, match=6):
+    return [row['date'] for idx, row in df.iterrows() if np.sum(np.array(row['winning_numbers']) == np.array(winning_nums)) == match]
+
+
+def _max_sum_date_from_dates(df, start, end):
+    start = datetime.datetime.strptime(start, '%m/%d/%Y')
+    end = datetime.datetime.strptime(end, '%m/%d/%Y')
+    df0 = df.loc[(df['date'] >= start) & (df['date'] <= end)]
+    max_sum = max([sum(i) for i in df0['winning_numbers']])
+    return [row['date'] for idx, row in df0.iterrows() if sum(row['winning_numbers']) == max_sum]
+
+
+def _max_freq_nums_from_dates(df, start, end):
+    start = datetime.datetime.strptime(start, '%m/%d/%Y')
+    end = datetime.datetime.strptime(end, '%m/%d/%Y')
+    df0 = df.loc[(df['date'] >= start) & (df['date'] <= end)]
+
+    num_freq_map = {}
+    for idx, row in df0.iterrows():
+        for num in row['winning_numbers']:
+            if num not in num_freq_map:
+                num_freq_map[num] = 0
+            num_freq_map[num] += 1
+
+    return sorted([num for num in num_freq_map.keys()], key=lambda x: num_freq_map[x], reverse=True)[:6]
 
 
 if __name__ == '__main__':
-    _populate_draws_list()
-    # pprint(_get_winning_nums_from_date(start='09/26/2020', end='10/03/2020'))
-    # pprint(_get_draws_from_multiplier(10))
-    # app.run(debug=True)
-    pass
+    df = _dataframe_from_file()
